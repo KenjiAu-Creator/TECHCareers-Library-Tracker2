86681d533856eb32bcb011b5be8fc1c4a36b2601
Kenji <kenji.au9@gmail.com>
2020-11-02 13:13:38 -0700

    Initial Commit;

---
+++ LIbrary/Controllers/BookController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LIbrary.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LIbrary.Controllers
+{
+  public class BookController : Controller
+  {
+    public static List<Book> Books = new List<Book>();
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Create()
+    {
+      return View();
+    }
+
+    public IActionResult List()
+    {
+      return View();
+    }
+
+    public IActionResult Details()
+    {
+      return View();
+    }
+    
+    // Methods
+    public static void CreateBook(int _id, string _title, string _author, DateTime _pubDate, DateTime _checkedOutDate)
+    {
+      // This method will create a Book object and add it to the Books list.
+      Book newBook = new Book(_id, _title, _author, _pubDate, _checkedOutDate);
+      Books.Add(newBook);
+    }
+
+    public static Book GetBookByID(int _id)
+    {
+      // This method will return a Book object with the given ID from the Books list.
+      return Books.Where(x => x.ID == _id).SingleOrDefault();
+    }
+
+    public static void ExtendDueDateForBookByID()
+    {
+      // This method will extend the given book with the IDs due date by 7 days.
+    }
+
+    public static void ReturnBookByID()
+    {
+      // This method will set the ReturnedDate of the Book object with the given ID to the current date.
+    }
+    public static void DeleteBookByID(int _id)
+    {
+      // This method will remove the Book object with the given ID from the Books list.
+      Books.Remove(GetBookByID(_id));
+    }
+  }
+}
---
+++ LIbrary/Models/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LIbrary.Models
+{
+  public class Book
+  {
+    public int ID { get; set; }
+    public string Title { get; set; }
+
+    public DateTime PublicationDate { get; set; }
+
+    public DateTime DueDate { get; set; }
+
+    public DateTime ReturnedDate { get; set; }
+    public string Author { get; set; }
+
+    public Book(int _id, string _title, string _author, DateTime _pubDate, DateTime _checkedOutDate)
+    {
+
+      this.ID = _id;
+      this.Title = _title;
+      this.Author = _author;
+      this.PublicationDate = _pubDate;
+
+      // The due date should be set to 14 days after the checkout.
+      this.DueDate = _checkedOutDate;
+    }
+  }
+}
