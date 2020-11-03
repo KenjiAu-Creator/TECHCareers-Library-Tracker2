d833ea353f741d0d0c5b65b3362a35f729716fd6
Kenji <kenji.au9@gmail.com>
2020-11-02 19:51:58 -0700

    Book properties set to private / read;

--- LIbrary/Models/Book.cs
+++ LIbrary/Models/Book.cs
@@ -7,16 +7,16 @@ namespace LIbrary.Models
 {
   public class Book
   {
-    public int ID { get; set; }
-    public string Title { get; set; }
+    public int ID { get; private set; }
+    public string Title { get; private set; }
 
-    public DateTime PublicationDate { get; set; }
-    public DateTime CheckedOutDate { get; set; }
+    public DateTime PublicationDate { get; private set; }
+    public DateTime CheckedOutDate { get; private set; }
 
     public DateTime DueDate { get; set; }
 
     public DateTime ReturnedDate { get; set; }
-    public string Author { get; set; }
+    public string Author { get; private set; }
 
     public Book(int _id, string _title, string _author, DateTime _pubDate, DateTime _checkedOutDate)
     {
